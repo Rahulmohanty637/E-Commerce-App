34a64ab58e0aa27eeb1024f5b77bb3d0e7dfeb88
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2023-11-05 11:02:48 +0530

    Homepage and navigation

---
+++ e-commerce/src/customer/Pages/HomePage/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import MainCarousel from "../../component/HomeCarousel/MainCarousel";
+
+const Homepage = () => {
+    return (
+        <div>
+            <MainCarousel></MainCarousel>
+        </div>
+    )
+}
+export default Homepage;
---
+++ e-commerce/src/customer/component/HomeCarousel/MainCarouselData.js
@@ -0,0 +1,22 @@
+export const mainCarouseldata = [
+  {
+    image:
+      "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/3/_/3_12.jpg",
+    path: "",
+  },
+  {
+    image:
+      "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/7/_/7_8.jpg",
+    path: "",
+  },
+  {
+    image:
+      "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/3/_/3_12.jpg",
+    path: "",
+  },
+  {
+    image:
+      "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/7/_/7_8.jpg",
+    path: "",
+  },
+];
